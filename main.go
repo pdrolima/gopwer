8e4e531270c3c97b677fcd2325b711300290fd46
webmasterdro <pdro.lima@pm.me>
2022-01-24 02:55:30 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	app.Listen(":3000")
+}
