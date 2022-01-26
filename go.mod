81a0fba1095a690ae13ae7d97c27ae6420526481
webmasterdro <pdro.lima@pm.me>
2022-01-26 02:03:46 -0300

    :white_check_mark: Add test for hashing

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/webmasterdro/gopwer
 go 1.17
 
 require (
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/go-playground/validator/v10 v10.10.0
 	github.com/gofiber/fiber/v2 v2.25.0
 	github.com/gofiber/jwt/v3 v3.2.3
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
-	github.com/lib/pq v1.10.4
 	gorm.io/driver/mysql v1.2.3
 	gorm.io/gorm v1.22.5
 )
--- hashing/hashing.go
+++ hashing/hashing.go
@@ -11,19 +11,19 @@ import (
 func HashPassword(username string, password string) (string, error) {
 	switch config.Config("PASSWORD_HASH") {
 	case "md5":
-		return hashPasswordMD5(username, password)
+		return HashPasswordMD5(username, password)
 	case "base64md5":
-		return hashPasswordBase64MD5(username, password)
+		return HashPasswordBase64MD5(username, password)
 	}
 	return "", nil
 }
 
-func hashPasswordMD5(username string, password string) (string, error) {
+func HashPasswordMD5(username string, password string) (string, error) {
 	hash := md5.Sum([]byte(username + password))
 	return "0x" + hex.EncodeToString(hash[:]), nil
 }
 
-func hashPasswordBase64MD5(username string, password string) (string, error) {
+func HashPasswordBase64MD5(username string, password string) (string, error) {
 	hash := md5.New()
 	io.WriteString(hash, username+password)
 	return base64.StdEncoding.EncodeToString(hash.Sum(nil)), nil
@@ -32,19 +32,19 @@ func hashPasswordBase64MD5(username string, password string) (string, error) {
 func CheckPassword(username string, password string, hashedPassword string) (bool, error) {
 	switch config.Config("PASSWORD_HASH") {
 	case "md5":
-		return checkPasswordMD5(username, password, hashedPassword), nil
+		return CheckPasswordMD5(username, password, hashedPassword), nil
 	case "base64md5":
-		return checkPasswordBase64MD5(username, password, hashedPassword), nil
+		return CheckPasswordBase64MD5(username, password, hashedPassword), nil
 	}
 	return false, nil
 }
 
-func checkPasswordBase64MD5(username string, password string, hashedPassword string) bool {
-	hash, _ := hashPasswordBase64MD5(username, password)
+func CheckPasswordBase64MD5(username string, password string, hashedPassword string) bool {
+	hash, _ := HashPasswordBase64MD5(username, password)
 	return hash == hashedPassword
 }
 
-func checkPasswordMD5(username string, password string, hashedPassword string) bool {
-	hash, _ := hashPasswordMD5(username, password)
+func CheckPasswordMD5(username string, password string, hashedPassword string) bool {
+	hash, _ := HashPasswordMD5(username, password)
 	return hash == hashedPassword
 }
